9903bf1c0e550f1df25a8f69d49be722e255ee25
intanrahma <intanrahmasafira@gmail.com>
2025-09-16 13:50:17 +0700

    pesan commit

---
+++ lib/data/user_data.dart
@@ -0,0 +1,10 @@
+/// Variabel global untuk menyimpan data pengguna.
+/// Struktur: Map<String, Map<String, String>>
+/// Key luar adalah email, value adalah Map yang berisi fullName dan password.
+Map<String, Map<String, String>> userData = {
+  // Contoh data awal (bisa dikosongkan)
+  'intanrahmasafira@email.com': {
+    'fullName': 'intanrahmasafira',
+    'password': '311007',
+  },
+};
